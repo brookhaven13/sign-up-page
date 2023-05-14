5f4651159c5ab59ec5eef9fd16f6e51f898cd486
Harley <yu.h.lin1991@gmail.com>
2023-05-15 01:45:16 +0800

    complete form validators

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.scss']
+  styleUrls: ['./sign-up.component.scss'],
 })
 export class SignUpComponent {
+  signUpForm!: FormGroup;
+  isPasswordVisible: boolean = false;
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm() {
+    this.signUpForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.pattern(/\d/), // check at least one number
+        ],
+      ],
+    });
+  }
+
+  submitForm() {
+    if (this.signUpForm.valid) {
+      console.log('form value', this.signUpForm.value);
+    } else {
+      console.log('error, please fill all the field before submitting form');
+    }
+  }
+
+  get password() {
+    return this.signUpForm.get('password');
+  }
 }
