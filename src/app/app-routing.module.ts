edaf0a56ea09c91dad0b6c65906fff6ed2806fdd
Harley <yu.h.lin1991@gmail.com>
2023-05-20 20:37:09 +0800

    update routng

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { SignUpComponent } from './sign-up/sign-up.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'sign-up', pathMatch: 'full' },
-  { path: 'sign-up', component: SignUpComponent },
+  { path: '', component: SignUpComponent },
 ];
 
 @NgModule({
